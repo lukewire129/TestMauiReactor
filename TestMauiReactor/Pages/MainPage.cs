dcef725ac0a016b9d678c33ae8b30e747ee62703
Luke.Lee <xlwpdldhk@gmail.com>
2023-05-29 22:56:09 +0900

    초기 커밋.

---
+++ TestMauiReactor/Pages/MainPage.cs
@@ -0,0 +1,28 @@
+﻿using MauiReactor;
+
+namespace TestMauiReactor.Pages
+{
+    internal class MainPageState
+    {
+        public int Counter { get; set; }
+    }
+
+    internal class MainPage : Component<MainPageState>
+    {
+        public override VisualNode Render()
+        {
+            return new ContentPage
+            {
+                new ScrollView
+                {
+                    new VerticalStackLayout
+                    {
+                    }
+                    .VCenter()
+                    .Spacing(25)
+                    .Padding(30, 0)
+                }
+            };
+        }
+    }
+}
